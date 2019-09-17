acbe0681a7bab69ddf8df5c6b1c59e2945403fb5
Link Dupont <link@sub-pop.net>
2019-09-17 15:09:32 -0400

    Begin documentation around Unmarshal

--- decode.go
+++ decode.go
@@ -24,6 +24,15 @@ func (e *InvalidUnmarshalError) Error() string {
 // Unmarshal parses the INI data and stores the result in the value pointed to
 // by v. If v is nil or not a pointer to a struct, Unmarshal returns an
 // error.
+//
+// To unmarshal a section into a struct, Unmarshal matches incoming property keys
+// to the keys used by Marshal (either the struct field name or its tag). Property
+// keys which do not have a corresponding struct field are ignored.
+//
+// To unmarshal a property into an int, int64, uint, or uint64, the value must
+// successfully parse (via strconv.Parse*) to a number.
+//
+// To unmarshal a property into a string, a direct string value is copied.
 func Unmarshal(data []byte, v interface{}) error {
 	rv := reflect.ValueOf(v)
 	if rv.Kind() != reflect.Ptr || rv.IsNil() {
