b4f1bb33da6c805d647c020bd2ab520975029265
Link Dupont <link@sub-pop.net>
2019-09-17 15:27:00 -0400

    Isolate TestMarshal

--- encode_test.go
+++ encode_test.go
@@ -6,72 +6,30 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-type owner struct {
-	Name         string `ini:"name"`
-	Organization string `ini:"organization"`
-}
-type database struct {
-	Server  string `ini:"server"`
-	Port    int    `ini:"port"`
-	File    string `ini:"file"`
-	Enabled bool   `ini:"enabled,omitempty"`
-}
-
-type config struct {
-	Version  string   `ini:"version,omitempty"`
-	Owner    owner    `ini:"owner"`
-	Database database `ini:"database"`
-}
-
 func TestMarshal(t *testing.T) {
+	type database struct {
+		Server    string
+		Port      int
+		Encrypted bool
+	}
+
 	tests := []struct {
-		input config
-		want  []byte
+		input struct {
+			Database database
+		}
+		want []byte
 	}{
 		{
-			input: config{
-				Version: "1.2.3",
-				Owner: owner{
-					Name:         "John Doe",
-					Organization: "Acme Widgets Inc.",
-				},
+			input: struct {
+				Database database
+			}{
 				Database: database{
-					Server: "192.0.2.62",
-					Port:   143,
-					File:   "\"payroll.dat\"",
+					Server:    "192.0.2.62",
+					Port:      143,
+					Encrypted: false,
 				},
 			},
-			want: []byte(`version=1.2.3
-
-[owner]
-name=John Doe
-organization=Acme Widgets Inc.
-
-[database]
-server=192.0.2.62
-port=143
-file="payroll.dat"`),
-		},
-		{
-			input: config{
-				Owner: owner{
-					Name:         "John Doe",
-					Organization: "Acme Widgets Inc.",
-				},
-				Database: database{
-					Server: "192.0.2.62",
-					Port:   143,
-					File:   "\"payroll.dat\"",
-				},
-			},
-			want: []byte(`[owner]
-name=John Doe
-organization=Acme Widgets Inc.
-
-[database]
-server=192.0.2.62
-port=143
-file="payroll.dat"`),
+			want: []byte("[Database]\nServer=192.0.2.62\nPort=143\nEncrypted=false"),
 		},
 	}
 
