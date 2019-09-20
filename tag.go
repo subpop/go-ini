3a5c772877fb9df392bdd8bf6b1b997806e0dc05
Link Dupont <link@sub-pop.net>
2019-09-20 16:11:10 -0400

    Correctly handle case where tag is empty

--- tag.go
+++ tag.go
@@ -14,10 +14,11 @@ func newTag(sf reflect.StructField) tag {
 	var t tag
 	st := strings.SplitN(sf.Tag.Get("ini"), ",", 2)
 	switch len(st) {
-	case 0:
-		t.name = sf.Name
 	case 1:
 		t.name = st[0]
+		if t.name == "" {
+			t.name = sf.Name
+		}
 	default:
 		t.name = st[0]
 		t.omitempty = strings.Contains(st[1], "omitempty")
