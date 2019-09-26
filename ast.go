9e5803fcf786b8d06e2e9b217ae8e29eb85f7fe5
Link Dupont <link@sub-pop.net>
2019-09-26 12:25:58 -0400

    add more logic to ast

--- ast.go
+++ ast.go
@@ -1,5 +1,26 @@
 package ini
 
+import (
+	"fmt"
+	"regexp"
+)
+
+type missingSectionErr struct {
+	name string
+}
+
+func (e *missingSectionErr) Error() string {
+	return fmt.Sprintf("parse error: missing section name %q", e.name)
+}
+
+type missingPropertyErr struct {
+	key string
+}
+
+func (e *missingPropertyErr) Error() string {
+	return fmt.Sprintf("parse error: missing property key %q", e.key)
+}
+
 type ast map[string][]section
 
 // newAST returns an AST containing a single-element, global property section.
@@ -16,6 +37,27 @@ func (a ast) addSection(s section) {
 	}
 	sec = append(sec, s)
 	a[s.name] = sec
+}
+
+func (a ast) getSection(name string) ([]section, error) {
+	sections, ok := a[name]
+	if !ok {
+		return nil, &missingSectionErr{name}
+	}
+	return sections, nil
+}
+
+func (a ast) getSectionMatch(r *regexp.Regexp) []section {
+	sections := make([]section, 0)
+	for name, section := range a {
+		if name == "" {
+			continue
+		}
+		if r.MatchString(name) {
+			sections = append(sections, section...)
+		}
+	}
+	return sections
 }
 
 type section struct {
@@ -38,6 +80,14 @@ func (s section) addProperty(p property) {
 	prop.val = append(prop.val, p.val...)
 
 	s.props[p.key] = prop
+}
+
+func (s section) getProperty(key string) (*property, error) {
+	prop, ok := s.props[key]
+	if !ok {
+		return nil, &missingPropertyErr{key}
+	}
+	return &prop, nil
 }
 
 type property struct {
